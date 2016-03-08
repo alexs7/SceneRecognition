5062edbf8847406de114c6fd023ca538b2bce5cb
alex <alexandrosrotsides@gmail.com>
2016-03-08 15:24:40 +0000

    initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+import org.openimaj.image.MBFImage;
+
+/**
+ * Created by alex on 08/03/2016.
+ */
+public class App {
+
+    public static void main(String[] args){
+        MBFImage image = null;
+        System.out.println("fuck you");
+    }
+
+}
