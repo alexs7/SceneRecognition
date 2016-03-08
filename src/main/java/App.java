792e1eb7312e4ae5554fe8e35792fc7c5b76570d
alex <alexandrosrotsides@gmail.com>
2016-03-08 16:35:15 +0000

    minor fixes and testing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,8 @@
+import org.openimaj.image.ImageUtilities;
 import org.openimaj.image.MBFImage;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by alex on 08/03/2016.
@@ -7,7 +11,11 @@ public class App {
 
     public static void main(String[] args){
         MBFImage image = null;
-        System.out.println("fuck you");
+        try {
+            image = ImageUtilities.readMBF(new File("/Users/alex/Projects/University Notes/COMP6223 Computer Vision/CW3/training/Coast/0.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
