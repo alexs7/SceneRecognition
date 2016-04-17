2bdced9edbdfc5ae8a3164e0b9df730682ab9abc
alex <alexandrosrotsides@gmail.com>
2016-04-17 21:33:43 +0100

    refactored PHOWExtractor

--- src/main/java/com/alexs7/PHOWExtractor.java
+++ src/main/java/com/alexs7/PHOWExtractor.java
@@ -2,11 +2,14 @@ package com.alexs7;
 
 import org.openimaj.feature.DoubleFV;
 import org.openimaj.feature.FeatureExtractor;
+import org.openimaj.feature.SparseIntFV;
 import org.openimaj.feature.local.list.LocalFeatureList;
 import org.openimaj.image.FImage;
 import org.openimaj.image.feature.dense.gradient.dsift.DenseSIFT;
 import org.openimaj.image.feature.dense.gradient.dsift.FloatDSIFTKeypoint;
 import org.openimaj.image.feature.dense.gradient.dsift.PyramidDenseSIFT;
+import org.openimaj.image.feature.local.aggregate.BagOfVisualWords;
+import org.openimaj.image.feature.local.aggregate.BlockSpatialAggregator;
 
 import java.util.ArrayList;
 
@@ -25,16 +28,23 @@ public class PHOWExtractor implements FeatureExtractor<DoubleFV, FImage> {
 
     @Override
     public DoubleFV extractFeature(FImage image) {
+        pdsift.analyseImage(image); //Compute the dense sift descriptors.
+        BagOfVisualWords<float[]> bovw = new BagOfVisualWords<float[]>(codeBook.getAssigner());
 
-        float[][] imageDescriptors = Utilities.getDenseSIFT(image,pdsift);
-        double[] bagOfVisualWordHistorgram = new double[codeBook.size()];
-        int representativeVectorIndex;
+        BlockSpatialAggregator<float[], SparseIntFV> spatial = new BlockSpatialAggregator<float[], SparseIntFV>(bovw, 2, 2);
 
-        for (float[] imageDescriptor : imageDescriptors){
-            representativeVectorIndex = codeBook.getRepresentativeVectorIndexFromDescriptor(imageDescriptor);
-            bagOfVisualWordHistorgram[representativeVectorIndex] +=1;
-        }
+        return spatial.aggregate(pdsift.getFloatKeypoints(), image.getBounds()).normaliseFV();
 
-        return new DoubleFV(bagOfVisualWordHistorgram);
+//        float[][] imageDescriptors = Utilities.getDenseSIFT(image,pdsift);
+//        double[] bagOfVisualWordHistogram = new double[codeBook.size()];
+//        int representativeVectorIndex;
+//
+//        for (float[] imageDescriptor : imageDescriptors){
+//            representativeVectorIndex = codeBook.getRepresentativeVectorIndexFromDescriptor(imageDescriptor);
+//            bagOfVisualWordHistogram[representativeVectorIndex] +=1;
+//        }
+//
+//        return new DoubleFV(bagOfVisualWordHistogram);
+
     }
 }
